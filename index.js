76fc4d68dac455b1b8dbd5136cdaadd6fcbf867b
Radames Vaz <rgvtovar@hotmail.com>
2020-04-05 18:31:25 -0400

    debugging

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('FUNCIOOOOOONAAAAAAaaaAAAaa') });
+app.get("/", (req, res) => { res.send('FUNCIOOOOOONAAAAAAaaaAAAaa') });
 
 //Home
 
