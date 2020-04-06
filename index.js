af8f20c031975bc2e0bfd4591aaca81b3999e30c
Radames Vaz <rgvtovar@hotmail.com>
2020-04-05 21:38:22 -0400

    didnt work

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ const home = require('./controllers/Home');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString : process.env.DATABASE_URL?ssl: true,
-     // connectionString : process.env.DATABASE_URL,
-     // ssl: true
+      connectionString : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
