1c40c551745e18e5a9b8e67034c6ba75cec4e63b
Radames Vaz <rgvtovar@hotmail.com>
2020-04-06 11:38:59 -0400

    hopefully

--- cloudinary.js
+++ cloudinary.js
@@ -4,7 +4,7 @@ const  dotenv = require('dotenv');
 dotenv.config();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
+    cloud_name: 'radames',
     api_key: 755953186971795,
     api_secret: '_H-Y5tD1ID4ozcZryu7OV_4Irns'
 });
