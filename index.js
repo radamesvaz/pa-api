94bd4eb07eadfbec0f534ac473f188ae85506ade
Radames Vaz <rgvtovar@hotmail.com>
2020-04-05 21:35:22 -0400

    idk

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const home = require('./controllers/Home');
 const db = knex({
     client: 'pg',
     connection: {
+        connectionString : process.env.DATABASE_URL?ssl: true,
      // connectionString : process.env.DATABASE_URL,
      // ssl: true
     }
