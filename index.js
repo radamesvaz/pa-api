7a509051d6ec9153dae6ef3969fb9141119d1432
Radames Vaz <rgvtovar@hotmail.com>
2020-04-05 20:25:21 -0400

    cant find the bug

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const home = require('./controllers/Home');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: 'postgresql-curved-05112',
         ssl: true,
     }
   });
